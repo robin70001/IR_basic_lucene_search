3028c4df7bad32651ccb9a579a9e8d44fe7b5895
robin70001 <robin70001@gmail.com>
2018-03-03 04:51:16 +0000

    Change v6 - removed comments

--- src/main/java/grp1/art1/IndexFiles.java
+++ src/main/java/grp1/art1/IndexFiles.java
@@ -42,15 +42,11 @@ public class IndexFiles {
 
 	private IndexFiles() {
 	}
-
-	// public static void main(String[] args) throws IOException {
+	
 	public static void cranIndexer(String indPath, String docPath, Boolean iwcSim, int valAnalyzer) throws IOException {
-		// String indexPath = "C:\\Users\\robin\\Desktop\\TCD\\4 IR & web
-		// String docsPath = "C:\\Users\\robin\\Desktop\\TCD\\4 IR & web
 		String indexPath = indPath;
 		String docsPath = docPath;
 
-		// System.out.println("docspath is :" + docsPath );
 		final Path docDir = Paths.get(docsPath);
 		if (!Files.isReadable(docDir)) {
 			System.out.println("Document directory '" + docDir.toAbsolutePath()
@@ -59,11 +55,7 @@ public class IndexFiles {
 		}
 
 		try {
-			// System.out.println("Indexing to directory '" + indexPath + "'...");
-
 			Directory dir = FSDirectory.open(Paths.get(indexPath));
-			
-	
 			Analyzer analyzer = null;
 			// Anlyzer choosing
 			if (valAnalyzer == 1) {
@@ -73,86 +65,47 @@ public class IndexFiles {
 			} else if (valAnalyzer == 3) {
 				analyzer = new SimpleAnalyzer();
 			} 
-			// Analyzer analyzer = new StandardAnalyzer();
-			// Analyzer analyzer = new EnglishAnalyzer(); // ENGLISH ANALYSER FOR DIFFERENT
-/*
-			Analyzer analyzer = CustomAnalyzer.builder().withTokenizer(StandardTokenizerFactory.class).addTokenFilter(StandardFilterFactory.class).addTokenFilter(LowerCaseFilterFactory.class).addTokenFilter(PorterStemFilterFactory.class).build();
-*/			
+		
 			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
-			iwc.setOpenMode(OpenMode.CREATE); // Always creates a new index everytime you run this
+			// Always creates a new index everytime you run this
+			iwc.setOpenMode(OpenMode.CREATE); 
 
+			//Choosing Similarity
 			if (iwcSim == true) {
 				iwc.setSimilarity(new BM25Similarity());
 			} else {
 				iwc.setSimilarity(new ClassicSimilarity());
 			}
 			IndexWriter writer = new IndexWriter(dir, iwc);
-			// indexDocs(writer, docDir);
-			//writer.flush();
-			
+		
 			cranIndex(writer, docsPath);
-			// NOTE: if you want to maximize search performance,
-			// you can optionally call forceMerge here. This can be
-			// a terribly costly operation, so generally it's only
-			// worth it when your index is relatively static (ie
-			// you're done adding documents to it):
-			//
-			//writer.forceMerge(0); // MAYBE USEFUL
-
+			
 			writer.close();
-			// System.out.println("finished");
 
 		} catch (IOException e) {
 			System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
 		}
+		System.out.println("---- Index Generated ----");
 	}
 
 	static void cranIndex(final IndexWriter writer, String cranPath) throws IOException {
 		Scanner sc = new Scanner(new File(cranPath));
 		sc.useDelimiter(".I");
 		while (sc.hasNext()) {
-			// System.out.println("split shown");
-			// System.out.println(sc.next());
 			String chunk = sc.next();
-
-			// System.out.println(chunk);
-
 			String[] ops = chunk.split(".I|.T|.A|.B|.W");
-			// System.out.println("Splits 1");
 			// System.out.println(ops[0]);
-			// System.out.println(ops[1]);
-			// System.out.println(ops[2]);
-			// System.out.println(ops[3]);
-			// System.out.println(ops[4]);
-
 			Document doc = new Document();
-			// System.out.println(chunk);
+
 			// chunk split with .T .A .W .B
-			
 			doc.add(new TextField("id", ops[0].trim(), Field.Store.YES));
 			doc.add(new TextField("title", ops[1].trim(), Field.Store.YES));
 			doc.add(new TextField("author", ops[2].trim(), Field.Store.YES));
 			doc.add(new TextField("bibliography", ops[3].trim(), Field.Store.YES));
 			doc.add(new TextField("content", ops[4].trim(), Field.Store.YES));
-			
-			
-			// System.out.println("\n \n ************** \n \n");
-			//writer.addDocument(doc);
-			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-				// New index, so we just add the document (no old document can be there):
-				// System.out.println("adding " + cranPath);
-
-				writer.addDocument(doc);}
-//			} else {
-//				// Existing index (an old copy of this document may have been indexed) so
-//				// we use updateDocument instead to replace the old one matching the exact
-//				// path, if present:
-//				// System.out.println("updating " + cranPath);
-//				writer.updateDocument(new Term("path", cranPath.toString()), doc);
-//			}
+	
+			writer.addDocument(doc);
 		}
 		sc.close();
-		//writer.close();
 	}
-
 }
